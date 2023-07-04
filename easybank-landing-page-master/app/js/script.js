ec25fcace122488b31b4928c5396e024439c6ec1
younusraza909 <younusraza909@gmail.com>
2023-07-04 10:47:42 +0500

    overlay fade-out animation added

--- easybank-landing-page-master/app/js/script.js
+++ easybank-landing-page-master/app/js/script.js
@@ -1,6 +1,15 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
 
 btnHamburger.addEventListener("click", function () {
-  header.classList.toggle("open");
+  if (header.classList.contains("open")) {
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  } else {
+    header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+  }
 });
