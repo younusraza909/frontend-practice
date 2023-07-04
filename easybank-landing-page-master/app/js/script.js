977f51253117694946c13ece6671641fa4d3e780
younusraza909 <younusraza909@gmail.com>
2023-07-04 09:59:45 +0500

    added js for hamburger menu

--- easybank-landing-page-master/app/js/script.js
+++ easybank-landing-page-master/app/js/script.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+const btnHamburger = document.querySelector("#btnHamburger");
+
+btnHamburger.addEventListener("click", function () {
+  btnHamburger.classList.toggle("open");
+});
