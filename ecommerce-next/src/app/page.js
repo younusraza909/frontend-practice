77c10aa17f020e70c34435c71d41ef78ba13d4ee
younusraza909 <younusraza909@gmail.com>
2023-08-22 15:48:30 +0500

    Mobile Navgation

--- ecommerce-next/src/app/page.js
+++ ecommerce-next/src/app/page.js
@@ -1,48 +1,19 @@
-import Image from 'next/image';
+'use client';
+
+import Navbar from '@/components/Navbar';
+import { useState } from 'react';
 
 export default function Home() {
+  const [mobileNav, setMobileNav] = useState(false);
   return (
     <main className='max-w-7xl mx-auto max-xl:max-w-5xl max-lg:max-w-3xl max-md:px-10'>
-      <nav className='flex items-center justify-between py-10 border-b-[1px]  max-md:border-none'>
-        <div className='left-nav flex items-center'>
-          <Image
-            src='./assets/images/icon-menu.svg'
-            width={20}
-            height={20}
-            alt='Menu Icon'
-            className='mr-10 hidden max-md:block'
-          />
-          <Image
-            src='./assets/images/logo.svg'
-            width={150}
-            height={150}
-            alt='Logo Sneakers'
-            className='mr-10 '
-          />
-
-          <div className='navlinks max-md:hidden'>
-            <ul className='flex items-center justify-between text-[color:var(--grayish-blue)] text-md tracking-tighter'>
-              <li className='mx-2'>Collections</li>
-              <li className='mx-2'>Men</li>
-              <li className='mx-2'>Women</li>
-              <li className='mx-2'>About</li>
-              <li className='mx-2'>Contact</li>
-            </ul>
-          </div>
-        </div>
-        <div className='right-nav flex items-center gap-16'>
-          <div className='w-6 h-6 rounded-full relative'>
-            <Image src='./assets/images/icon-cart.svg' fill alt='Cart Logo' />
-          </div>
-          <div className='w-12 h-12 rounded-full relative'>
-            <Image
-              src='/assets/images/image-avatar.png'
-              fill
-              alt='Profile Logo Avatar'
-            />
-          </div>
-        </div>
-      </nav>
+      {mobileNav && (
+        <div className='bg-[#000] bg-opacity-50 z-10 w-full h-full absolute inset-0' />
+      )}
+      <Navbar
+        showMobileNav={mobileNav}
+        handleToggleNav={(val) => setMobileNav(val)}
+      />
     </main>
   );
 }
---
+++ ecommerce-next/src/components/Navbar.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import Image from 'next/image';
+
+function Navbar({ showMobileNav, handleToggleNav }) {
+  return (
+    <nav className='flex items-center justify-between py-10 border-b-[1px]  max-md:border-none'>
+      <div className='left-nav flex items-center'>
+        <Image
+          src='./assets/images/icon-menu.svg'
+          width={20}
+          height={20}
+          alt='Menu Icon'
+          onClick={() => handleToggleNav(true)}
+          className='mr-10 hidden max-md:block cursor-pointer'
+        />
+        <Image
+          src='./assets/images/logo.svg'
+          width={150}
+          height={150}
+          alt='Logo Sneakers'
+          className='mr-10 '
+        />
+
+        <div className='navlinks max-md:hidden'>
+          <ul className='flex items-center justify-between text-[color:var(--grayish-blue)] text-md tracking-tighter'>
+            <li className='mx-2'>Collections</li>
+            <li className='mx-2'>Men</li>
+            <li className='mx-2'>Women</li>
+            <li className='mx-2'>About</li>
+            <li className='mx-2'>Contact</li>
+          </ul>
+        </div>
+      </div>
+      <div className='right-nav flex items-center gap-16 max-md:gap-10'>
+        <div className='w-6 h-6 rounded-full relative'>
+          <Image src='./assets/images/icon-cart.svg' fill alt='Cart Logo' />
+        </div>
+        <div className='w-12 h-12 max-md:w-8 max-md:h-8 rounded-full relative'>
+          <Image
+            src='/assets/images/image-avatar.png'
+            fill
+            alt='Profile Logo Avatar'
+          />
+        </div>
+      </div>
+
+      {/* Mobile Navigation Sidebar */}
+      {showMobileNav && (
+        <div className='bg-white z-30 h-full absolute top-0 left-0 bottom-0 w-[60%]'>
+          <div className='w-4 h-4 m-10  relative text-[color:var(--primary-color)]'>
+            <Image
+              src='/assets/images/icon-close.svg'
+              fill
+              alt='Close Icon'
+              className='cursor-pointer'
+              onClick={() => handleToggleNav(false)}
+            />
+          </div>
+
+          <ul className='m-10 flex items-start gap-10 font-[700] text-xl flex-col'>
+            <li>Collections</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+}
+
+export default Navbar;
