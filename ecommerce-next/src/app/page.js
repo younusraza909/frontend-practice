758b147ba40b8f8a5fee07cbf388682ccb373edd
younusraza909 <younusraza909@gmail.com>
2023-08-22 15:09:03 +0500

    Basic Desktop Nav

--- ecommerce-next/src/app/page.js
+++ ecommerce-next/src/app/page.js
@@ -2,38 +2,35 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main>
-      <nav className='flex items-center justify-between'>
-        <div className='left-nav'>
+    <main className='max-w-7xl mx-auto'>
+      <nav className='flex items-center justify-between py-10  border-b-[1px]'>
+        <div className='left-nav flex items-center'>
           <Image
             src='./assets/images/logo.svg'
             width={150}
             height={150}
             alt='Logo Sneakers'
+            className='mr-10'
           />
 
           <div className='navlinks'>
-            <ul>
-              <li>Collections</li>
-              <li>Men</li>
-              <li>Women</li>
-              <li>About</li>
-              <li>Contact</li>
+            <ul className='flex items-center justify-between text-[color:var(--grayish-blue)] text-md tracking-tighter'>
+              <li className='mx-2'>Collections</li>
+              <li className='mx-2'>Men</li>
+              <li className='mx-2'>Women</li>
+              <li className='mx-2'>About</li>
+              <li className='mx-2'>Contact</li>
             </ul>
           </div>
         </div>
-        <div className='right-nav'>
-          <Image
-            src='./assets/images/icon-cart.svg'
-            width={50}
-            height={50}
-            alt='Cart Logo'
-          />
-          <div className='profile-image'>
+        <div className='right-nav flex items-center gap-16'>
+          <div className='w-6 h-6 rounded-full relative'>
+            <Image src='./assets/images/icon-cart.svg' fill alt='Cart Logo' />
+          </div>
+          <div className='w-12 h-12 rounded-full relative'>
             <Image
               src='/assets/images/image-avatar.png'
-              width={60}
-              height={60}
+              fill
               alt='Profile Logo Avatar'
             />
           </div>
