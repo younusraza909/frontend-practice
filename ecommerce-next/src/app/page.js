58ce93589367a2d238edc6d00980f0b18ac802be
younusraza909 <younusraza909@gmail.com>
2023-08-23 16:15:45 +0500

    Setting responsivnes on device other than mobile

--- ecommerce-next/src/app/page.js
+++ ecommerce-next/src/app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
         handleToggleNav={(val) => setMobileNav(val)}
       />
 
-      <section className='product flex items-center gap-20 my-20 mx-10'>
+      <section className='product  max-lg:flex-col flex items-center max-xl:gap-6 gap-20 my-20 mx-10'>
         <div className='image-showcase flex-1 '>
           <div className='main-product-image relative h-[450px] w-[90%]  rounded-xl overflow-hidden'>
             <Image src='/assets/images/image-product-1.jpg' fill />
@@ -92,17 +92,19 @@ export default function Home() {
                 </p>
               </div>
             </div>
-            <div className=' cursor-pointer card-cta-btn bg-[color:var(--primary-color)] text-white px-10 rounded-lg py-3 flex items-center gap-11'>
-              <div className='relative w-4 h-4'>
+            <div className=' cursor-pointer card-cta-btn bg-[color:var(--primary-color)] text-white px-10 rounded-lg py-3 max-xl:py-0 flex items-center  justify-between gap-11'>
+              <div className=''>
                 <svg width='22' height='20' xmlns='http://www.w3.org/2000/svg'>
                   <path
                     d='M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z'
                     fill='#fff'
-                    fill-rule='nonzero'
+                    fillRule='nonzero'
                   />
                 </svg>
               </div>
-              <p className='text-sm font-bold'>Add to card</p>
+              <p className='whitespace-nowrap text-sm font-bold max-xl:text-xs'>
+                Add to card
+              </p>
             </div>
           </div>
         </div>
