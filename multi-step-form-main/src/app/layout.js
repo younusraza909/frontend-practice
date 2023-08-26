28a1920bc119fa614c2048f6d2bdd8834cd12cfb
younusraza909 <younusraza909@gmail.com>
2023-08-26 10:02:47 +0500

    Basic Setup for Multi Step Form Challenge

---
+++ multi-step-form-main/src/app/layout.js
@@ -0,0 +1,17 @@
+import './globals.css';
+import { Ubuntu } from 'next/font/google';
+
+const ubuntu = Ubuntu({ subsets: ['latin'], weight: ['400', '500', '700'] });
+
+export const metadata = {
+  title: 'Multi Step Form App',
+  description: 'Frontend Mentor Practice Project',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body className={ubuntu.className}>{children}</body>
+    </html>
+  );
+}
