8620dd1c1e22b89348adafab937762e62c860e53
younusraza909 <younusraza909@gmail.com>
2023-08-28 10:58:44 +0500

    Basic Form Setup done

--- multi-step-form-main/src/app/layout.js
+++ multi-step-form-main/src/app/layout.js
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={ubuntu.className}>{children}</body>
+      <body className={`${ubuntu.className} bg-[color:var(--light-gray)]`}>
+        {children}
+      </body>
     </html>
   );
 }
--- multi-step-form-main/src/app/page.js
+++ multi-step-form-main/src/app/page.js
@@ -1,5 +1,18 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <main className=''></main>;
+  return (
+    <main className='flex items-center justify-center h-[100vh]'>
+      <div className='form-box w-[70vw]  bg-white flex  h-[700px] p-4 rounded-md'>
+        <div className='desktop-left-side flex-1'>
+          <div className='w-full h-full'></div>
+        </div>
+        <div className='desktop-right-side flex-[3]'>
+          <div className='mx-32 my-16'>
+            <p>Hello </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
