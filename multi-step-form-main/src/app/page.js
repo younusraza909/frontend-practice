d1be1a66d01da27ff810366d3134b6eb5e604dfa
younusraza909 <younusraza909@gmail.com>
2023-08-28 11:01:17 +0500

    basic input field

--- multi-step-form-main/src/app/page.js
+++ multi-step-form-main/src/app/page.js
@@ -9,7 +9,19 @@ export default function Home() {
         </div>
         <div className='desktop-right-side flex-[3]'>
           <div className='mx-32 my-16'>
-            <p>Hello </p>
+            <h1>Personal info</h1>
+            <p>Please provide your name, email address, and phone number.</p>
+
+            <div class='w-full max-w-md mx-auto p-4'>
+              <label class='block mb-2 text-gray-700' for='inputField'>
+                Label:
+              </label>
+              <input
+                id='inputField'
+                type='text'
+                class='w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-200 focus:border-blue-500'
+              />
+            </div>
           </div>
         </div>
       </div>
