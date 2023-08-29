e4ab258a3cd2a34f60801c8181737a5e297bb49d
younusraza909 <younusraza909@gmail.com>
2023-08-29 11:07:18 +0500

    Personal Step Content Done

--- multi-step-form-main/src/app/page.js
+++ multi-step-form-main/src/app/page.js
@@ -1,27 +1,74 @@
-import Image from 'next/image';
+'use client';
+
+import { useState } from 'react';
 
 export default function Home() {
+  const [step, setStep] = useState(1);
+
+  const stepInfo = [
+    {
+      title: 'Your Info',
+    },
+    {
+      title: 'Select Plan',
+    },
+    {
+      title: 'Add-ons',
+    },
+    {
+      title: 'Summary',
+    },
+  ];
+
   return (
     <main className='flex items-center justify-center h-[100vh]'>
       <div className='form-box w-[70vw]  bg-white flex  h-[700px] p-4 rounded-md'>
         <div className='desktop-left-side flex-1'>
-          <div className='w-full h-full'></div>
+          <div className='w-full h-full m-9'>
+            <div className='flex flex-col gap-10'>
+              {stepInfo.map((s, index) => (
+                <div className='flex items-center gap-5'>
+                  <div className={`step-number`}>{index + 1}</div>
+                  <div className='flex flex-col'>
+                    <p className='text-[color:var(--light-gray)] text-md'>
+                      STEP {index + 1}
+                    </p>
+                    <p className='text-white  text-lg font-bold tracking-wider'>
+                      {s.title}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
         <div className='desktop-right-side flex-[3]'>
           <div className='mx-32 my-16'>
-            <h1>Personal info</h1>
-            <p>Please provide your name, email address, and phone number.</p>
+            <h1 className='primary-heading'>Personal info</h1>
+            <p className='sub-text'>
+              Please provide your name, email address, and phone number.
+            </p>
 
-            <div class='w-full max-w-md mx-auto p-4'>
-              <label class='block mb-2 text-gray-700' for='inputField'>
-                Label:
+            <div className='w-full  mx-auto mb-10'>
+              <label className='input-label' for='name'>
+                Name
               </label>
-              <input
-                id='inputField'
-                type='text'
-                class='w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-200 focus:border-blue-500'
-              />
+              <input id='name' type='text' className='input' />
             </div>
+            <div className='w-full  mx-auto  mb-10'>
+              <label className='input-label' for='email'>
+                Email Address
+              </label>
+              <input id='email' type='text' className='input' />
+            </div>
+            <div className='w-full  mx-auto  mb-10'>
+              <label className='input-label' for='phone'>
+                Phone Number
+              </label>
+              <input id='phone' type='text' className='input' />
+            </div>
+
+            <div className='btn-primary'>Next Step</div>
           </div>
         </div>
       </div>
